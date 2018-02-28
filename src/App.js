fff6a60057ceadda8e6c7ccaa43024f0fc97ae7b
EmmanuelCBaby <emmanuel.b@westagilelabs.com>
2018-02-28 14:56:49 +0530

    css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './App.css'
 import axios from 'axios'
 
 class App extends Component {
@@ -6,10 +7,12 @@ class App extends Component {
         super(props);
         this.state = {
           keyword:'',
-          details:[]
+          details:[],
+          category:'movie'
         }
         this.foundkeyword=this.foundkeyword.bind(this);
         this.search=this.search.bind(this);
+        this.getCategory=this.getCategory.bind(this);
     }
 
     //sets the keyword state to whatever you print in the search text field
@@ -23,7 +26,7 @@ class App extends Component {
     search(){
       axios({
         method:'get',
-        url:`https://api.themoviedb.org/3/search/movie?api_key=618beb7230424a9d83ef94b33f200275&query=${this.state.keyword}`
+        url:`https://api.themoviedb.org/3/search/${this.state.category}?api_key=618beb7230424a9d83ef94b33f200275&query=${this.state.keyword}`
       })
       .then((obj)=>{
         this.setState({
@@ -35,24 +38,35 @@ class App extends Component {
       })
     }
 
+    //gets the category for the search from the combo list
+    getCategory(event){
+      this.setState({
+        category:event.target.value
+      })
+    }
+
     render() {
       return (
         <div>
-            <input type='text' placeholder='Find Movies, TV shows, Celebrities and more...' onChange={this.foundkeyword} value={this.state.keyword}></input>
-            <br/>
-            <br/>
+            <input type='text' placeholder='Search...' onChange={this.foundkeyword} value={this.state.keyword}></input>
+            <select onChange={this.getCategory}>
+              <option value='movie'>Movie</option>
+              <option value='person'>Actor/Actress</option>
+              <option value='person'>Director</option>
+              <option value='person'>Producer</option>
+            </select>
             <button type='button' onClick={this.search}>Search</button>
             <div>
               {
                 this.state.details.map((element,index)=>{
-                  var poster = `https://image.tmdb.org/t/p/w92/${element.poster_path}`
+                  var poster = `https://image.tmdb.org/t/p/w92/${element.poster_path}`;
                   return(
-                    <div key={index}>
-                    <h2>{element.title}</h2>
-                    <img src={poster} alt='img' />
-                    <h6>Language: {element.original_language}</h6>
-                    <h6>Rating: {element.vote_average}</h6>
-                    <h6>Release Date: {element.release_date}</h6>
+                    <div className='movieList' key={index}>
+                    <img className='image' src={poster} alt='img' />
+                    <h2 className='movieTitle'>{element.title}</h2>
+                    <h6 className='movieLanguage'>Language: {element.original_language}</h6>
+                    <h6 className='movieRating'>Rating: {element.vote_average}</h6>
+                    <h6 className='movieReleaseDate'>Release Date: {element.release_date}</h6>
                     </div>
                   )
                 })
